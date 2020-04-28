c5555f20d625ed1f1de48014f4278f3dcdaa888f
jmkaneshiro <jmkaneshiro@gmail.com>
2020-04-28 13:45:43 -0400

    Adds squirrel icon and basic marker/popup styling

--- src/index.js
+++ src/index.js
@@ -8,6 +8,14 @@ window.addEventListener("DOMContentLoaded", () => {
   //Center of map set to 86th Street Central Park
   const squirrelMap = L.map('mapid').setView([40.782864, -73.965355], 15);
 
+  const brnSquirrelIcon = L.icon ({
+    iconUrl: "/assets/map_icons/brn-squirrel-icon.svg",
+
+    iconSize: [20, 20], // size of icon
+    iconAnchor: [10, 20], // set anchor relative to icon
+    popupAnchor: [5, -18] //set popup relative to anchor
+  });
+
   L.tileLayer(`https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=${MAPBOX_TOKEN}`, {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
@@ -16,4 +24,16 @@ window.addEventListener("DOMContentLoaded", () => {
     zoomOffset: -1,
     accessToken: 'your.mapbox.access.token'
   }).addTo(squirrelMap);
+
+  //Example for adding marker
+  L.marker([40.782864, -73.965355], {icon: brnSquirrelIcon}).addTo(squirrelMap).bindPopup('Hello!');
+
+  //Example for adding circle
+  // L.circle([40.782864, -73.965355], {
+  //   color: 'red',
+  //   fillColor: '#f03',
+  //   fillOpacity: 0.5,
+  //   radius: 300
+  // }).addTo(squirrelMap);
+
 });
