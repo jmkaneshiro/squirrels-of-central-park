cc07f21087545c36b72d755e3bca71f910018c39
jmkaneshiro <jmkaneshiro@gmail.com>
2020-04-29 12:50:12 -0400

    Adds basic filter to sort by color

--- src/components/map.js
+++ src/components/map.js
@@ -17,6 +17,25 @@ export function Map() {
         cinnamonSquirrelIcon = new SquirrelIcon({ iconUrl: "/assets/map_icons/cinnamon-squirrel-icon.svg" }),
         blackSquirrelIcon = new SquirrelIcon({ iconUrl: "/assets/map_icons/black-squirrel-icon.svg" });
 
+  function defaultPopup(feature, layer) {
+    const { primary_fur_color, shift, foraging, climbing, running } = feature.properties;
+
+    const dateString = feature.properties.date.replace(/(\d{2})(\d{2})(\d+)/, '$1/$2/$3');
+    const date = new Date(dateString);
+
+    layer.bindPopup(
+      `<h2>Details<h2>
+        <ul>
+          <li>Date: ${dateString}</li>
+          <li>Fur Color: ${primary_fur_color}</li>
+          <li>Shift: ${shift}</li>
+          <li>Foraging: ${foraging}</li>
+          <li>Running: ${running}</li>
+          <li>Climibing: ${climbing}</li>
+        </ul>`
+    );
+  }
+
   // Create base tile for map
   L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -33,7 +52,44 @@ export function Map() {
   xhr.open('GET', 'https://data.cityofnewyork.us/resource/vfnx-vebw.geojson');
   xhr.onload = () => {
     const data = JSON.parse(xhr.response);
-    const addedGeoJSON = L.geoJSON(data, {
+
+    const blackSquirrels = L.geoJSON(data, {
+      filter: function(feature, layer) {
+         return feature.properties.primary_fur_color === "Black";
+      },
+      pointToLayer: function (feature, latlng) {
+        return L.marker(latlng, {
+          icon: blackSquirrelIcon
+        });
+      },
+      onEachFeature: defaultPopup
+    });
+
+    const cinnamonSquirrels = L.geoJSON(data, {
+      filter: function (feature, layer) {
+        return feature.properties.primary_fur_color === "Cinnamon";
+      },
+      pointToLayer: function (feature, latlng) {
+        return L.marker(latlng, {
+          icon: cinnamonSquirrelIcon
+        });
+      },
+      onEachFeature: defaultPopup
+    });
+
+    const graySquirrels = L.geoJSON(data, {
+      filter: function (feature, layer) {
+        return feature.properties.primary_fur_color === "Gray";
+      },
+      pointToLayer: function (feature, latlng) {
+        return L.marker(latlng, {
+          icon: graySquirrelIcon
+        });
+      },
+      onEachFeature: defaultPopup
+    });
+
+    const allSquirrels = L.geoJSON(data, {
       //Set squirrel icon based on geoJSON property primary_fur_color
       pointToLayer: function (feature, latlng) {
         const primaryFurColor = feature.properties.primary_fur_color;
@@ -44,29 +100,43 @@ export function Map() {
         } else {
           return L.marker(latlng, { icon: graySquirrelIcon });
         }
-      },
-      onEachFeature: function (feature, layer) {
-        const { primary_fur_color, shift, foraging, climbing, running } = feature.properties;
-
-        const dateString = feature.properties.date.replace(/(\d{2})(\d{2})(\d+)/, '$1/$2/$3');
-        const date = new Date(dateString);
-
-        layer.bindPopup(
-          `<h2>Details<h2>
-          <ul>
-            <li>Date: ${dateString}</li>
-            <li>Fur Color: ${primary_fur_color}</li>
-            <li>Shift: ${shift}</li>
-            <li>Foraging: ${foraging}</li>
-            <li>Running: ${running}</li>
-            <li>Climibing: ${climbing}</li>
-          </ul>`
-        );
       }
-    }).addTo(squirrelMap);
+    });
 
-    squirrelMap.fitBounds(addedGeoJSON.getBounds(), {
+    squirrelMap.fitBounds(allSquirrels.getBounds(), {
       padding: [20,20]
+    });
+
+    squirrelMap.addLayer(blackSquirrels);
+    squirrelMap.addLayer(cinnamonSquirrels);
+    squirrelMap.addLayer(graySquirrels);
+    //change squirrel filter on click
+    const filterFurBlack = document.getElementById('filter-fur-black');
+    const filterFurCinnamon = document.getElementById('filter-fur-cinnamon');
+    const filterFurGray = document.getElementById('filter-fur-gray');
+
+    filterFurBlack.addEventListener('change', function () {
+      if (this.checked) {
+        squirrelMap.addLayer(blackSquirrels);
+      } else {
+        squirrelMap.removeLayer(blackSquirrels);
+      }
+    });
+
+    filterFurCinnamon.addEventListener('change', function () {
+      if (this.checked) {
+        squirrelMap.addLayer(cinnamonSquirrels);
+      } else {
+        squirrelMap.removeLayer(cinnamonSquirrels);
+      }
+    });
+
+    filterFurGray.addEventListener('change', function () {
+      if (this.checked) {
+        squirrelMap.addLayer(graySquirrels);
+      } else {
+        squirrelMap.removeLayer(graySquirrels);
+      }
     });
   };
   xhr.send();
